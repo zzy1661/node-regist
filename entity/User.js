c35c7164c54f63a3fbed3342968d77329ce1b3e3
zzy1661 <1556597051@qq.com>
2018-12-22 05:02:34 +0800

    用户构造器修改

--- entity/User.js
+++ entity/User.js
@@ -1,9 +1,11 @@
+var {md5} = require('../sec');
+
 class User {
 
-    constructor({id,name,pw,role,email}) {
+    constructor({id,name,pw,role,email,rawPw}) {
         this.id = id;
         this.name = name;
-        this.pw = pw,
+        this.pw = pw || md5(rawPw),
         this.role = role;
         this.email = email;    
     }
