9753294f13cc257b60b5aab95ed36c30165e8613
zzy1661 <1556597051@qq.com>
2018-12-22 00:48:54 +0800

    增加taskDao的测试

---
+++ tests/testDao/test.js
@@ -0,0 +1,26 @@
+var TaskDao = require('../../dao/TaskDao');
+var Task = require('../../entity/Task');
+var taskDao = new TaskDao();
+// taskDao.queryTasks().then(tasks=>{
+//     taskDao.deleteTask(tasks[0]).then(results=>{
+//         console.log('delete',results);
+//     })
+// })
+/* var newTask = new Task({name:`task${Math.random().toFixed(2)}`});
+taskDao.addTask(newTask).then((result)=>{
+    console.log('add',result);
+}).catch(err=>{
+    console.log(err);
+}) */
+taskDao.queryTaskById({id:1}).then(task=>{
+    task.name = 'newtask';
+    console.log(task);
+    taskDao.updateTask(task).then(results=>{
+        console.log('update',results);
+    }).catch(err=>{
+        console.log(err);
+    })
+}).catch(err=>{
+    console.log(err);
+})
+// console.log(new Task({id:1}))
